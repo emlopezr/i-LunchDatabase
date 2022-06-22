cd90453f501fd1758fe6dfb3c4f6c0e45e670a64
Emmanuel López <lopezrodemmanuel@gmail.com>
2022-06-21 19:58:36 -0500

    Página de inicio

--- i-Lunch/index.php
+++ i-Lunch/index.php
@@ -1,6 +1,55 @@
 <?php
-
 $paginaActiva = 0;
 include "includes/header.php";
+?>
 
+<div class="mt-5">
+    <!-- Información del trabajo -->
+    <div class="row">
+        <div class="col-1 px2"></div>
+
+        <div class="col-6 px2">
+            <img src="images/Logo.png" alt="Logo i-Lunch" class="img-fluid">
+        </div>
+
+        <div class="col-4 px2">
+            <h1>Trabajo final de Bases de Datos I</h1>
+
+            <!-- Integrantes -->
+            <div class="mt-3">
+                <h3>Integrantes</h3>
+                <ul>
+                    <li>Andrés Felipe Aparicio Mestre - <a href="mailto:anapariciom@unal.edu.co">anapariciom@unal.edu.co</a></li>
+                    <li>David Esteban Toro Arango - <a href="mailto:datoro@unal.edu.co">datoro@unal.edu.co</a></li>
+                    <li>Emmanuel López Rodríguez - <a href="mailto:emlopezr@unal.edu.co">emlopezr@unal.edu.co</a></li>
+                </ul>
+            </div>
+
+            <!-- Informacion materia -->
+            <div class="mt-3">
+                <h3>Información</h3>
+                <ul>
+                    <li><b>Materia:</b> Bases de Datos I</li>
+                    <li><b>Profesor: </b>Francisco Javier Moreno Arboleda</li>
+                    <li><b>Institución:</b> Universidad Nacional de Colombia sede Medellín</li>
+                    <li><b>Semestre:</b> 2022-1</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- Consulta 1 -->
+    <div class="row">
+        <div class="col-1 px2"></div>
+
+        <div class="col-10 px2">
+            <!-- Imagen i-Lunch -->
+        </div>
+    </div>
+
+
+</div>
+
+<?php
 include "includes/footer.php";
+?>

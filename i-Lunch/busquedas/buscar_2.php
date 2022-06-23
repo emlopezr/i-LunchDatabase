a049fdf2520ab122877c1a6f4073495e68f7c616
Emmanuel López <lopezrodemmanuel@gmail.com>
2022-06-23 10:56:07 -0500

    Mensaje de error búsqueda 2

--- i-Lunch/busquedas/buscar_2.php
+++ i-Lunch/busquedas/buscar_2.php
@@ -5,6 +5,13 @@ include "../includes/header.php";
 
 $n1 = $_POST["n1"];
 $n2 = $_POST["n2"];
+
+$correcto = False;
+
+if ($n1 < $n2) {
+    $correcto = True;
+}
+
 $query = "SELECT nit, nombre FROM(SELECT nit, nombre, numero_veces FROM (SELECT franquicia.nit, franquicia.nombre, COUNT(*) AS numero_veces FROM restaurante, franquicia WHERE restaurante.franquicia_duena = franquicia.nit GROUP BY franquicia.nit) AS T) AS T1 WHERE numero_veces BETWEEN '$n1' AND '$n2'";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 ?>
@@ -16,43 +23,51 @@ $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
         <div class="col-4 px-2">
             <h2>Busqueda 2</h2>
             <p class="text-justify">El usuario ingresa dos numeros enteros N1 y N2. Se cumple que 0 ≤ N1 < N2. Se muestra el NIT y el nombre de todas las franquicias que son dueñas de entre N1 y N2 restaurantes (Intervalo cerrado [N1, N2]).</p>
-            <a href="busqueda_2.php" class="btn btn-primary">Regresar a la pagina de la búsqueda</a>
+                    <a href="busqueda_2.php" class="btn btn-primary">Regresar a la pagina de la búsqueda</a>
         </div>
 
-        <div class="col-6 px-2">
-            <!-- Tabla de restaurantes -->
-            <table class="table border-rounded">
-                <!-- Cabecera de la tabla -->
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">NIT</th>
-                        <th scope="col">Nombre</th>
-                    </tr>
-                </thead>
+        <?php if ($correcto) : ?>
+            <div class="col-6 px-2">
+                <!-- Tabla de restaurantes -->
+                <table class="table border-rounded">
+                    <!-- Cabecera de la tabla -->
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">NIT</th>
+                            <th scope="col">Nombre</th>
+                        </tr>
+                    </thead>
 
-                <!-- Cuerpo de la tabla -->
-                <tbody>
-                    <!-- Iterar sobre los restaurantes -->
-                    <?php
-                    if ($result) :
-                        foreach ($result as $fila) :
-                    ?>
+                    <!-- Cuerpo de la tabla -->
+                    <tbody>
+                        <!-- Iterar sobre los restaurantes -->
+                        <?php
+                        if ($result) :
+                            foreach ($result as $fila) :
+                        ?>
 
-                            <!-- Fila -->
-                            <tr>
-                                <!-- Columnas -->
-                                <td><?= $fila["nit"]; ?></td>
-                                <td><?= $fila["nombre"]; ?></td>
+                                <!-- Fila -->
+                                <tr>
+                                    <!-- Columnas -->
+                                    <td><?= $fila["nit"]; ?></td>
+                                    <td><?= $fila["nombre"]; ?></td>
 
 
-                            </tr>
+                                </tr>
 
-                    <?php
-                        endforeach;
-                    endif;
-                    ?>
-                </tbody>
-            </table>
-        </div>
+                        <?php
+                            endforeach;
+                        endif;
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php else : ?>
+            <div class="col-6 px-2 alert alert-danger" role="alert">
+                Hubo un error en la búsqueda. Recuerda que N1 < N2.
+                <br>
+                Por favor regresa a la pagina anterior y vuelve a intentarlo.
+            </div>
+        <?php endif; ?>
     </div>
 </div>
